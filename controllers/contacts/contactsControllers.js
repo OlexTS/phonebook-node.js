0956b390f6fa38e2e7bb28102342222a12f30a75
OlexTS <sashokseven@gmail.com>
2023-06-17 16:30:14 +0300

    changed two ctrl

--- controllers/contacts/contactsControllers.js
+++ controllers/contacts/contactsControllers.js
@@ -1,6 +1,17 @@
-const getAllContactsCtrl = async () => {};
-const addContactCtrl = async () => {};
+const Contact =require('../../db/models/contactModel')
+
+const getAllContactsCtrl = async (req, res, next) => {
+    const result = await Contact.find();
+    res.json(result)
+};
+const addContactCtrl = async (req, res, next) => {
+    const result = await Contact.create(req.body);
+    console.log(result);
+    res.status(201).json(result)
+};
 const updateContactCtrl = async () => {};
 const deleteContactCtrl = async () => {};
+
+
 
 module.exports = { getAllContactsCtrl, addContactCtrl, updateContactCtrl, deleteContactCtrl };

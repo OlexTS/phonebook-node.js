f8f011899517d8cc67bef9e0d27c7c85f0ae5182
OlexTS <sashokseven@gmail.com>
2023-06-24 00:05:58 +0300

    created register ctrl

--- controllers/contacts/authControllers.js
+++ controllers/contacts/authControllers.js
@@ -1,7 +1,36 @@
+const bcrypt = require("bcrypt");
+// const jwt = require("jsonwebtoken");
+// const { nanoid } = require("nanoid");
+const { HttpError, ctrlWrapper } = require("../../helpers");
+const { User } = require("../../db/models/authModel");
 
-const register = async (req, res, next) => { };
-const logIn = async (req, res, next) => { };
-const logOut = async (req, res, next) => { };
-const refresh = async (req, res, next) => { };
+const register = async (req, res, next) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (user) {
+    throw HttpError(409, "Email already exists");
+  }
+  const createHashPassword = await bcrypt.hash(password, 10);
+  //   const verificationToken = nanoid();
+  const newUser = await User.create({
+    ...req.body,
+    password: createHashPassword,
+  });
 
-module.exports = {register, logIn, logOut, refresh};
+  res.status(201).json({
+    user: {
+      name: newUser.name,
+      email: newUser.email,
+    },
+  });
+};
+const logIn = async (req, res, next) => {};
+const logOut = async (req, res, next) => {};
+const refresh = async (req, res, next) => {};
+
+module.exports = {
+  register: ctrlWrapper(register),
+  logIn: ctrlWrapper(logIn),
+  logOut: ctrlWrapper(logOut),
+  refresh: ctrlWrapper(refresh),
+};
